2c60aebd3aff47e233155a96047f26a1e2ad0027
Paul Thorman <paulthorman@Pauls-MacBook-Pro.local>
2017-09-29 11:17:12 -0700

    almost done

---
+++ javascript/app.js
@@ -0,0 +1,250 @@
+$(document).ready(function(){
+	//Game object
+var triviaGame = {
+
+//Array t hold question and answers
+	qAndA:[{
+		question: "Who said, \"Hey, I could call my ma from up here!\"?",
+			ans1: "Mayor Quimby",
+			ans2: "Cletus",
+			ans3: "Principle Skinner",
+			ans4: "Ralph Wiggum",
+			imgUrl: "images/cletus.jpg"},
+	   {
+	   	question: "What is Reverend Lovejoy\'s first name?",
+			ans1: "Timothy",
+			ans2: "Barney",
+			ans3: "Carl",
+			ans4: "Lenny",
+			imgUrl: "images/lovejoy.jpeg"},
+		{
+	   	question: "Which Simpsons catchphrase was officially admitted into the Oxford English Dictionary in 2001?",
+		    ans1: "D\'oh!",
+		    ans2: "Ay Caramba",
+		    ans3: "Why, you little...",
+		    ans4: "Hi-Diddly-Ho!",
+			imgUrl: "images/homer.jpg"},
+	   {
+	   	question: "\'The Simspsons\' began as a short segment on what 1980s variety show?",
+			ans1: "Saturday Night Live",
+			ans2: "The Tracey Ullman Show",
+			ans3: "Fridays",
+			ans4: "In Living Color",
+			imgUrl: "images/original-simpsons.jpg"},
+		{
+		question: "Who holds back the electric car? Who made Steve Guttenberg a star?",
+		    ans1: "The Masons",
+		    ans2: "The Elks",
+		    ans3: "The Stonecutters",
+		    ans4: "The Illuminati",
+			imgUrl: "images/stonecutters.png"},],
+
+	correctAnswers: ['Cletus', 'Timothy', 'D\'oh!', 'The Tracey Ullman Show', 'The Stonecutters'],//array to hold correct answers
+	userAnswers: [],
+
+	questionCount: 0,
+	beginInt: 0,
+
+	timer: 30,
+	btnClicked: false,
+	numberCorrect: 0,
+	numberIncorrect: 0,
+	numberUnAnswered: 0,
+	playMusic: new Audio("images/maggie.mp3"),
+
+	beginGame: function(){
+		triviaGame.playMusic.play();	
+		if(triviaGame.questionCount == triviaGame.qAndA.length){
+
+			triviaGame.gameFinished();
+			triviaGame.timer = 30;
+
+		} else {
+
+			if(triviaGame.questionCount >= 1){
+				clearInterval(triviaGame.displayNextInt);
+				$('#gameStart').show();
+				$('#divAnswers').hide();
+				triviaGame.timer = 30;
+				$('#time').html(triviaGame.timer); //??
+			}
+
+			$('p.questions').html(triviaGame.qAndA[triviaGame.questionCount].question);
+			$('button.answer1').html(triviaGame.qAndA[triviaGame.questionCount].ans1);
+			$('button.answer2').html(triviaGame.qAndA[triviaGame.questionCount].ans2);
+			$('button.answer3').html(triviaGame.qAndA[triviaGame.questionCount].ans3);
+			$('button.answer4').html(triviaGame.qAndA[triviaGame.questionCount].ans4);
+
+			triviaGame.beginInt = setInterval(triviaGame.countDown, 1000);
+
+		}
+
+	},
+//Cound down timer 
+	countDown: function(){
+
+		triviaGame.timer--;
+		$('#time').html(triviaGame.timer);
+
+		if(triviaGame.timer == 0){
+
+			triviaGame.oufOfTime();
+			triviaGame.playMusic.pause();
+
+		} else if(triviaGame.btnClicked == true && triviaGame.correctAnswers[triviaGame.questionCount] == triviaGame.userAnswers[triviaGame.questionCount]){
+		
+			triviaGame.answersCorrect();
+			triviaGame.playMusic.play();
+
+		} else if(triviaGame.btnClicked == true && triviaGame.correctAnswers[triviaGame.questionCount] != triviaGame.userAnswers[triviaGame.questionCount]){
+
+			triviaGame.answersWrong();
+			triviaGame.playMusic.pause();
+		}
+
+	},
+//If option by player is correct
+	answersCorrect: function(){
+
+		if(newImg != ""){
+
+			$('#pic').empty();
+		}
+
+		$('#divAnswers').show();
+		$('#gameStart').hide();
+		$('#outOfTime').hide();
+		$('#wrongMsg').hide();	
+		$('#correctMsg').show();
+		$('#pCorrectAnswer').hide();	
+		$('#answers').css('display', 'block');
+		$('#timeRemaining').css('display', 'block');
+		$('#elapsedTime').html(triviaGame.timer);
+
+		clearInterval(triviaGame.beginInt);
+
+		var newImg = $("<img>").attr('src', triviaGame.qAndA[triviaGame.questionCount].imgUrl).attr('width', '130px','height', 'auto').attr('id', 'correctImage');
+
+		$('#pic').append(newImg);		
+		triviaGame.btnClicked = false;
+
+		triviaGame.displayNextInt = setInterval(triviaGame.beginGame, 3000);
+		triviaGame.numberCorrect++;
+		triviaGame.questionCount++;
+	},
+//if options by user is incorrect
+	answersWrong: function(){
+
+		if(newImg != ""){
+
+			$('#pic').empty();
+		}
+
+		$('#divAnswers').show();
+		$('#gameStart').hide();
+		$('#outOfTime').hide();
+		$('#wrongMsg').show();
+		$('#correctMsg').hide();
+		$('#pCorrectAnswer').show();
+		$('#pCorrectAnswer span').html(triviaGame.correctAnswers[triviaGame.questionCount]);
+		$('#timeRemaining').css('display', 'block');
+		$('#elapsedTime').html(triviaGame.timer);
+		clearInterval(triviaGame.beginInt);
+
+		var newImg = $("<img>").attr('src', triviaGame.qAndA[triviaGame.questionCount].imgUrl).attr('width', '115px').attr('id', 'correctImage');
+
+		$('#pic').append(newImg);
+
+		triviaGame.btnClicked = false;
+		triviaGame.displayNextInt = setInterval(triviaGame.beginGame, 5000);
+		triviaGame.numberIncorrect++;
+		triviaGame.questionCount++;
+	},
+//If the player is out of time 
+	oufOfTime: function(){
+
+		if(newImg != ""){
+
+			$('#pic').empty();
+		}
+
+		triviaGame.userAnswers.push(""); 
+		$('#divAnswers').show();
+		$('#gameStart').hide();
+		$('#pCorrectAnswer span').html(triviaGame.correctAnswers[triviaGame.questionCount]);
+		$('#pCorrectAnswer').show();
+		$('#correctMsg').hide();
+		$('#wrongMsg').hide();		
+		$('#timeRemaining').css('display', 'block');
+		$('#elapsedTime').html(triviaGame.timer);	
+		clearInterval(triviaGame.beginInt);
+		var newImg = $("<img>").attr('src', triviaGame.qAndA[triviaGame.questionCount].imgUrl).attr('width', '115px').attr('id', 'correctMovieImage');
+
+		$('#pic').append(newImg);
+
+		triviaGame.numberUnAnswered++;
+
+		triviaGame.displayNextInt = setInterval(triviaGame.beginGame, 5000);
+
+		triviaGame.questionCount++;	
+
+	},
+//Restart function
+	restart: function(){
+
+		triviaGame.questionCount = 0;
+		triviaGame.userAnswers.length = 0;
+		$('#time').html("30");
+
+		triviaGame.beginGame();
+		$('#gameStart').show();
+		$('#gameComplete').hide();
+		$('#restartPlaceholder').css('display', 'none');
+		clearInterval(triviaGame.displayNextInt);
+		$('#elapsedTime').empty();
+		triviaGame.numberCorrect = 0;
+		triviaGame.numberIncorrect = 0;
+		triviaGame.numberUnAnswered = 0;
+	},
+//Game Ends - Resets the DOM
+	gameFinished: function(){
+
+		$('#restartPlaceholder').css('display', 'block');
+		$('#divAnswers').hide();
+		$('#gameStart').hide();
+
+		$('#gameComplete').css('display', 'block');
+
+		$('#gameOverCorrect span').html(triviaGame.numberCorrect);
+		$('#gameOverIncorrect span').html(triviaGame.numberIncorrect);
+		$('#unanswered span').html(triviaGame.numberUnAnswered);
+		triviaGame.timer = 30;
+	}
+};
+
+
+
+//Game begins on click of the start button 
+	$('#begin').on('click', function(){
+
+		$('div#gameStart').css('display', 'block');
+		$('#btnWrapper').css('display', 'none');
+		$('.questions').html(triviaGame.beginGame);
+
+	});
+//once the player hits options
+	$('.answers').on('click', function(){
+
+		triviaGame.userAnswers.push($(this).text());
+		triviaGame.btnClicked = true;
+
+	});
+//once the player hits restart - calls function restart
+	$('#restartPlaceholder').on('click', function(){
+
+		triviaGame.restart();
+		
+	});
+
+
+});
